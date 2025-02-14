e4391444e6d01e47eb63ee256923711104f27269
Waldt Kotze <waldt.kotze@standardbank.co.za>
2025-02-14 17:14:59 +0200

    Adding New years resolutions to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 import { BlackJackComponent } from './black-jack/black-jack.component';
 import { AboutComponent } from './about/about.component';
+import { ResolutionsComponent } from './resolutions/resolutions.component';
 
 export const routes: Routes = [
   { path: 'tic-tac-toe', component: TicTacToeComponent },
   { path: 'black-jack', component: BlackJackComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'resolutions', component: ResolutionsComponent },
   { path: '', redirectTo: '/tic-tac-toe', pathMatch: 'full' }, // Default route
   { path: '**', redirectTo: '/tic-tac-toe' }, // Wildcard route
 ];
---
+++ src/app/resolutions/resolutions.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-resolutions',
+  templateUrl: './resolutions.component.html',
+  styleUrls: ['./resolutions.component.scss'],
+  imports: [ CommonModule, FormsModule ],
+})
+export class ResolutionsComponent implements OnInit {
+  resolutions: string[] = [];
+  newResolution: string = '';
+  editingIndex: number | null = null;
+
+  ngOnInit() {
+    const storedResolutions = localStorage.getItem('resolutions');
+    if (storedResolutions) {
+      this.resolutions = JSON.parse(storedResolutions);
+    }
+
+    
+  }
+
+  addResolution() {
+    if (this.newResolution.trim()) {
+      this.resolutions.push(this.newResolution);
+      this.saveResolutions();
+      this.newResolution = '';
+    }
+  }
+
+  deleteResolution(index: number) {
+    this.resolutions.splice(index, 1);
+    this.saveResolutions();
+  }
+
+  clearResolutions() {
+    this.resolutions = [];
+    this.saveResolutions();
+  }
+
+  editResolution(index: number) {
+    this.newResolution = this.resolutions[index];
+    this.editingIndex = index;
+  }
+
+  private saveResolutions() {
+    localStorage.setItem('resolutions', JSON.stringify(this.resolutions));
+  }
+}
